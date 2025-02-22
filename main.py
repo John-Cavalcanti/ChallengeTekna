1ea5b0affacf3d3a8f768a326029eff9c78ea7ab
João Cavalcanti <jvictorao123@gmail.com>
2025-02-21 21:58:07 -0300

    done: creaed function that clear columns Situação and Nota para Aprovação Final

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
 
 maximum_absences = 15
+scopes = ['https://www.googleapis.com/auth/drive']
+credentials = Credentials.from_service_account_file(
+  'TeknaChallenge-chave.json', scopes=scopes
+)
+client = gspread.authorize(credentials)
 
 # functions
 def calculate_situacao_and_nota(row):
@@ -19,18 +24,11 @@ def calculate_situacao_and_nota(row):
     final_grade_for_approval = 100 - average
     return "Recuperação", round(final_grade_for_approval)
   
-  
+def clear_situacao_and_nota(row):
+  return "", ""
 
-scopes = ['https://www.googleapis.com/auth/drive']
-credentials = Credentials.from_service_account_file(
-  'TeknaChallenge-chave.json', scopes=scopes
-)
-
-client = gspread.authorize(credentials)
 sheet = client.open('Software Engineer - Challenge [João Cavalcanti]').sheet1
-
 dados = sheet.get_all_values()
-
 df = pd.DataFrame(dados)
 
 # defining columns and their content
@@ -40,5 +38,9 @@ df = df.iloc[3:]
 # converting columns content to numeric
 df[["Faltas","P1","P2","P3"]] = df[["Faltas","P1","P2","P3"]].apply(pd.to_numeric)
 df[["Situação","Nota para Aprovação Final"]] = df.apply(calculate_situacao_and_nota, axis=1, result_type='expand')
+
+print(df)
+
+df[["Situação","Nota para Aprovação Final"]] = df.apply(clear_situacao_and_nota, axis=1, result_type='expand')
 
 print(df)
